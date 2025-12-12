6d02b05cea81daf1bb0331a940fa012a619faff9
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-12 20:05:21 +0530

    starting working on designing the model

--- models/user.model.js
+++ models/user.model.js
@@ -2,5 +2,5 @@ import { integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
-
+  firstname:
 });
