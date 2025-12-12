083d07f23cbb5317d08bcb57f66f1ba2b477b5bf
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-12 21:29:27 +0530

    completed the signup route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,32 @@
 import express from "express"
 import {db } from "../db/index.js"
 import { usersTable } from "../models/users.model.js"
+import { createHmac, randomBytes } from "crypto"
 const router = express.Router()
 
 
-router.post("/sign-up",(req,res)=>{
+router.post("/sign-up",async (req,res)=>{
     const {firstname, lastname, email, password} = req.body()
 
-    const existingUser = db;
+    const [existingUser] = await db.select({
+        id:usersTable.id
+    }).from(existingUser).
+        where(eq(usersTable.email, email))
 })
+
+if(existingUser) return res.status(400).json({message:`user with this ${email} already existed`})
+
+    const salt = randomBytes(256).toString('hex')
+    const hashedPassword= createHmac('sha256', salt).update(password).digest('hex')
+    const [user] = await db.insert(usersTable).values({
+        email, 
+        lastname,
+        firstname,
+        salt,
+        password:hashedPassword
+    }).returning({id: usersTable.id})
+
+    return res.status(200).json({data :{userId: user.id}});
+
 
 export default router;
