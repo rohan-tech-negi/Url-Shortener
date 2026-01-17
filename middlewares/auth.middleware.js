9219fc5eb31e55019143d3790cfcaeba1d34264f
rohan-tech-negi <rohanwork953@gmail.com>
2026-01-18 00:32:01 +0530

    completed with all the creating of the auth middleware structure

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,3 +1,22 @@
-function authenticationMiddleware(req,res,next){
 
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @param {import("express").NextFunction} next 
+ * @returns 
+ */
+
+
+
+function authenticationMiddleware(req,res,next){
+    const authHeader = req.headers('authorization');
+
+    if(!authHeader){
+        return next()
+    }
+    if(!authHeader.startsWith('Bearer'))
+        return res.status(400).json({error:'Authorization header must start with this '})
+
+    const [_, token] = authHeader.split(' ');
 }
